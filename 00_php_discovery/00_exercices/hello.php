7340d10db01042ddb5c7d32951e7970d301c8f71
nabil-elma-dev <0105naelmahfoudi@student.epfc.eu>
2026-09-17 16:02:04 +0200

    feat(php_intro): exercice 1

---
+++ 00_php_discovery/00_exercices/hello.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome</title>
+</head>
+<body>
+    <h1> Welcome </h1>
+    <p>
+        <?php 
+            echo "Hello World";
+        ?>
+    </p>
+</body>
+</html>
